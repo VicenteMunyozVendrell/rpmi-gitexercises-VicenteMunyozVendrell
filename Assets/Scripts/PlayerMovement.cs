539819e9ee256b23695166e49f77901f8a61d000
Vicente Muñoz Vendrell <vmjob@hotmail.com>
2026-02-20 17:16:14 +0100

    Añadido el movimiento del jugador
    Añadido el movimiento del jugador

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Personaje moviendose");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * 0.1f);
+    }
+}
