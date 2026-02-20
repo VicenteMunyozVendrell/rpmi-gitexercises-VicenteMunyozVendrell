014540ae2e43faeb363cd797d5d9bce0d3ddf507
Vicente Muñoz Vendrell <vmjob@hotmail.com>
2026-02-20 19:04:41 +0100

    Modificado parametro

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float speed = 0.5f;
+    [SerializeField] private float speed = 1f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
